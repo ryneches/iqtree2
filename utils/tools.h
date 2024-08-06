343ab5c7bfd7b7908525a968cab180b453d28df6
Russell Neches <russell@vort.org>
2024-08-06 15:44:37 +0900

    revert std::byte for tools.h

--- utils/tools.h
+++ utils/tools.h
@@ -132,7 +132,6 @@ inline void _my_assert(const char* expression, const char *func, const char* fil
 #endif
 
 using namespace std;
-using byte = std::byte;
 
 
 #if	defined(USE_HASH_MAP) && GCC_VERSION < 40300 && !defined(_MSC_VER) && !defined(__clang__)
